d8c20232c3762f8415f54557a252160f177a6e1b
Mariuz Calvin <mariuzcalvin@hotmail.com>
2022-08-25 08:32:49 +0700

    remove env

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const formData = require('express-form-data');
 
 app.use(formData.parse());
 
-
-require('dotenv').config()
-process.env.TOKEN_SECRET = 'e6740d33b149004e3d34deae286b5da426c60604594d7c66c7a9e5b16a1edb85ecdd745dfc74a7689751d9430482213d8531b2c870f014c22c382b7523b21e0d';
-
 var cors = require("cors");
 
 app.use(cors()); // Use this after the variable declaration
